025729f6d1189631e4895f1eb82d5d4996ff4fbd
Jared P. Jones <jjones@uvora.com>
2015-09-17 17:15:43 -0500

    Added Category

--- Exercise3Group7/Exercise3Group7/NSString+StringVerification.m
+++ Exercise3Group7/Exercise3Group7/NSString+StringVerification.m
@@ -10,4 +10,28 @@
 
 @implementation NSString (StringVerification)
 
+- (BOOL)containsNumber {
+    return false;
+}
+
+- (BOOL)containsUpperCaseCharacter {
+    return false;
+}
+
+-(BOOL) hasPunctuation {
+    NSRange range = [self rangeOfCharacterFromSet:[NSCharacterSet
+                                                   punctuationCharacterSet]];
+    return range.location != NSNotFound;
+}
+- (BOOL)isValidEmail {
+    NSString *expression = @"^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[AZa-z]{2,4}$";
+    NSError *error = NULL;
+    NSRegularExpression *regex = [NSRegularExpression
+                                  regularExpressionWithPattern:expression options:NSRegularExpressionCaseInsensitive error:&error];
+    NSTextCheckingResult *match = [regex firstMatchInString:self options:0 range:NSMakeRange(0, [self length])];
+    if (match) { return true;
+    } else
+        return false;
+}
+
 @end
