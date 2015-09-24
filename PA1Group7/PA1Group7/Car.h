741d6c1d2cdc1b2ab78d0b9be8ee6b16f12fc0ff
Jared P. Jones <jjones@uvora.com>
2015-09-24 16:19:51 -0500

    Added Car

---
+++ PA1Group7/PA1Group7/Car.h
@@ -0,0 +1,13 @@
+//
+//  Car.h
+//  PA1Group7
+//
+//  Created by Jared Jones on 9/24/15.
+//  Copyright © 2015 team7. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+
+@end
---
+++ PA1Group7/PA1Group7/Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  PA1Group7
+//
+//  Created by Jared Jones on 9/24/15.
+//  Copyright © 2015 team7. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
