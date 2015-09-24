ea11176256277044f30bfe0f13dacd2d32c6d460
Jared P. Jones <jjones@uvora.com>
2015-09-24 16:06:30 -0500

    Initial Assignment Commit

---
+++ PA1Group7/PA1Group7/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PA1Group7
+//
+//  Created by Jared Jones on 9/24/15.
+//  Copyright © 2015 team7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PA1Group7/PA1Group7/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PA1Group7
+//
+//  Created by Jared Jones on 9/24/15.
+//  Copyright © 2015 team7. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
