d82fa7e0619371c3212f1012c86dcef260d20f95
Jared P. Jones <jjones@uvora.com>
2015-10-01 16:34:57 -0500

    Initial

---
+++ Exercise4Group7/Exercise4Group7/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Exercise4Group7
+//
+//  Created by Jared Jones on 10/1/15.
+//  Copyright © 2015 team7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
