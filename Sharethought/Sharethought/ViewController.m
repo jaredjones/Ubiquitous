c1a747eea2a38b225f7ab8df0fc1a16909c6b7c1
Faysal <fxsrt10@gmail.com>
2015-10-08 15:53:53 -0500

    Initial commit

---
+++ Sharethought/Sharethought/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ShareThought
+//
+//  Created by UH Game and Entrepreneurship on 10/8/15.
+//  Copyright © 2015 Team7. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
