9987694762a9d84b177963cf2ca919b89f443afe
Jared P. Jones <jjones@uvora.com>
2015-10-08 16:23:31 -0500

    More

---
+++ Exercise5Group7/Exercise5Group7/UserTableViewCell.m
@@ -0,0 +1,13 @@
+//
+//  UserTableViewCell.m
+//  Exercise4Group7
+//
+//  Created by Jared Jones on 10/1/15.
+//  Copyright © 2015 team7. All rights reserved.
+//
+
+#import "UserTableViewCell.h"
+
+@implementation UserTableViewCell
+
+@end
---
+++ Exercise5Group7/Exercise5Group7/UserTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserTableViewController.h
+//  Exercise4Group7
+//
+//  Created by Jared Jones on 10/1/15.
+//  Copyright © 2015 team7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserTableViewController : UITableViewController
+
+@end
