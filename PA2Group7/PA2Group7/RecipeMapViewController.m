4d082e6bee37376f58ee100d7c60beff6ab933e5
Faysal <fxsrt10@gmail.com>
2015-10-22 16:27:26 -0500

    Added Shit

---
+++ PA2Group7/PA2Group7/RecipeMapViewController.m
@@ -0,0 +1,22 @@
+//
+//  RecipeMapViewController.m
+//  PA2Group7
+//
+//  Created by UH Game and Entrepreneurship on 10/22/15.
+//  Copyright © 2015 ubicomp7. All rights reserved.
+//
+
+#import "RecipeMapViewController.h"
+
+@interface RecipeMapViewController ()
+
+@end
+
+@implementation RecipeMapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
---
+++ PA2Group7/PA2Group7/RecipeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecipeTableViewController.h
+//  PA2Group7
+//
+//  Created by UH Game and Entrepreneurship on 10/22/15.
+//  Copyright © 2015 ubicomp7. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RecipeTableViewController : NSViewController
+
+@end
