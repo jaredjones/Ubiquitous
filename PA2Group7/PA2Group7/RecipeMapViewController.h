51530138d09a809e31010aebeefa1c4f3a3b4eb7
Faysal <fxsrt10@gmail.com>
2015-10-22 17:31:00 -0500

    RecipeMapViewController

--- PA2Group7/PA2Group7/RecipeMapViewController.h
+++ PA2Group7/PA2Group7/RecipeMapViewController.h
@@ -7,7 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+#import "Recipe.h"
 
-@interface RecipeMapViewController : UIViewController
+@interface RecipeMapViewController : UIViewController <CLLocationManagerDelegate, MKMapViewDelegate>
+@property (nonatomic,strong) Recipe* recipe;
 
 @end
