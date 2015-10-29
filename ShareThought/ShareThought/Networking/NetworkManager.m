c9a0299230320ca0ea7145a678874dd75878d100
ubicomp7 <ubicomp7@UBIComp-7s-iMac.local>
2015-10-29 14:28:25 -0500

    added client side support for userName

--- ShareThought/ShareThought/Networking/NetworkManager.m
+++ ShareThought/ShareThought/Networking/NetworkManager.m
@@ -71,7 +71,7 @@
 }
 
 - (void)registerWithEmail: (NSString *)email withPassword: (NSString *)password withFirstName: (NSString *)fName withLastName: (NSString *)lName withAboutYou: (NSString *)aboutYou withUserName:(NSString *)userName{
-    NSString *packetBody = [NSString stringWithFormat:@"%c%s%c%s%c%s%c%s%c%s", (char)email.length, [email cStringUsingEncoding:NSUTF8StringEncoding], (char)password.length, [password cStringUsingEncoding:NSUTF8StringEncoding], (char)fName.length, [fName cStringUsingEncoding:NSUTF8StringEncoding], (char) lName.length, [lName cStringUsingEncoding:NSUTF8StringEncoding], (char)aboutYou.length, [aboutYou cStringUsingEncoding:NSUTF8StringEncoding]];
+    NSString *packetBody = [NSString stringWithFormat:@"%c%s%c%s%c%s%c%s%c%s%c%s", (char)email.length, [email cStringUsingEncoding:NSUTF8StringEncoding], (char)password.length, [password cStringUsingEncoding:NSUTF8StringEncoding], (char)fName.length, [fName cStringUsingEncoding:NSUTF8StringEncoding], (char) lName.length, [lName cStringUsingEncoding:NSUTF8StringEncoding], (char)aboutYou.length, [aboutYou cStringUsingEncoding:NSUTF8StringEncoding], (char)userName.length, [userName cStringUsingEncoding:NSUTF8StringEncoding]];
     NSData *packetBodyData = [packetBody dataUsingEncoding:NSUTF8StringEncoding];
     
     uint64_t finalSize;
