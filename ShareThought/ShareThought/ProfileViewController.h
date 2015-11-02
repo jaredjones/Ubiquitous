4de33151243a8c198ffa2a7f6718043fcf3d3cef
Tim Dickson <timothy.edward.dickson@gmail.com>
2015-11-02 14:39:03 -0600

    inital commit for profile veiw controller.

--- ShareThought/ShareThought/ProfileViewController.h
+++ ShareThought/ShareThought/ProfileViewController.h
@@ -2,15 +2,12 @@
 //  ProfileViewController.h
 //  ShareThought
 //
-//  Created by UH Game and Entrepreneurship on 10/12/15.
+//  Created by Tim Dickson on 11/2/15.
 //  Copyright © 2015 Team7. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-
 @interface ProfileViewController : UIViewController
-@property (nonatomic, strong) NSString *fullname;
-@property (nonatomic, strong) NSString *profileDescription;
-@property (nonatomic, strong) NSString (*tagArray);
+
 @end
