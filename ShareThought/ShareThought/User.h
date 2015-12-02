e84f0785ee88201a4c09b53f0d556a8d8e32e48c
Jared P. Jones <jjones@uvora.com>
2015-12-02 06:01:02 -0600

    Fixed User Class

--- ShareThought/ShareThought/User.h
+++ ShareThought/ShareThought/User.h
@@ -10,16 +10,15 @@
 
 @interface User : NSObject <NSCoding>
 
+@property (nonatomic, strong) NSString *username;
 @property (nonatomic, strong) NSString *email;
-@property (nonatomic, strong) NSString *password;
 @property (nonatomic, strong) NSString *fname;
 @property (nonatomic, strong) NSString *lname;
 @property (nonatomic, strong) NSString *profileDescription;
 
 
-- (instancetype)initWithUser: (NSString *)user withPassword: (NSString *)password withFirstName: (NSString *)firstName withLastName: (NSString *)lastName withProfileDesc: (NSString *) pDesc;
+- (instancetype)initWithUser: (NSString *)user withEmail: (NSString *)email withFirstName: (NSString *)firstName withLastName: (NSString *)lastName withProfileDesc: (NSString *) pDesc;
 + (BOOL)doesEmailExist: (NSString*) email;
-+ (BOOL)emailCredentialsValid: (NSString *)email withPassword: (NSString *)password;
 + (NSArray*) retrieveDataFromNSUserDefaults;
 + (void)storeDataInNSUserDefaults:(User *)userToStore;
 @end
